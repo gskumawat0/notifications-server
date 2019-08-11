edab79da1db8926dd8f0f6b40607492edf09e77c
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2019-08-11 14:17:03 +0530

    add socket.io

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
     .then((e) => console.log('connected to db'))
     .catch((err) => console.log(err.message)); 
 
+//cors config
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_APP_BASE_URL);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header('Access-Control-Allow-Credentials', true)
+    next();
+});
+
 //handle express errors
 app.use((err, req, res, next)=>{
     if(err){
@@ -24,6 +33,21 @@ app.use((err, req, res, next)=>{
         })
     }
     next();
+})
+
+app.use((req, res, next)=>{
+    req.io = io;
+    next();
+})
+
+
+
+io.on('connection', (socket)=>{
+    socket.broadcast.emit('add_notification', 'a new user connected')
+      
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('add_notification', 'user logged out')
+    })
 })
 
 server.listen(process.env.PORT, ()=>{
